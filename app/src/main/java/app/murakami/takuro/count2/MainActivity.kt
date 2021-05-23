bd2ad3fd14047d11ce2f71b4123f2def10dd4acc
takuro <67560179+nuaseTakuro@users.noreply.github.com>
2021-05-23 10:55:20 +0900

    プラスボタン完成

---
+++ app/src/main/java/app/murakami/takuro/count2/MainActivity.kt
@@ -0,0 +1,21 @@
+package app.murakami.takuro.count2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    var number = 0
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        plusButton.setOnClickListener {
+            number++
+            count.text = number.toString()
+        }
+
+
+
+    }
+}
